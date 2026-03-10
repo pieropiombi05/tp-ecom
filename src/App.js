cf49f6f255beb4d07484bdadc4482829c8ece9dd
Piero Piombi <ppiombi@itba.edu.ar>
2026-03-10 15:27:17 -0300

    Create src/App.js as the main React component for the clothing e-commerce store

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Clothing E-Commerce Store</h1>
+            <p>Find the best deals on your favorite outfits!</p>
+        </div>
+    );
+};
+
+export default App;
